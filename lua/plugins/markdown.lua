b8961274a71f5bf496a72f2da7ef11d151cb533c
Mohammad Hasan <hasanmohammad6618@gmail.com>
2026-05-10 03:08:14 +0530

    refactoring

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,6 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
 	config = function()
 		require("render-markdown").setup({
 			code = {
