401e0839444da0bcc87484dc75638bbb1727ce7d
Mohammad Hasan <hasanmohammad6618@gmail.com>
2026-06-09 22:28:12 +0530

    bring back stylua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
 			},
 			formatters_by_ft = {
 				cpp = { "clang-format" },
+				lua = { "stylua" },
 				python = { "ruff_format" },
 				-- Conform will run multiple formatters sequentially
 				-- python = { "isort", "black" },
