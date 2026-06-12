9a3e393aaacc15fdb57b8a1484c94796c8aaf8d2
Mohammad Hasan <hasanmohammad6618@gmail.com>
2026-06-12 21:52:55 +0530

    use my fork of lazydev and remove emmylua_ls config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,26 +2,6 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = { "saghen/blink.cmp" },
 	config = function()
-		vim.lsp.config("emmylua_ls", {
-			settings = {
-				emmylua = {
-					runtime = {
-						version = "LuaJIT",
-						requirePattern = {
-							"?.lua",
-							"?/init.lua",
-						},
-					},
-					workspace = {
-						library = {
-							{
-								path = "/usr/share/nvim/runtime/",
-							},
-						},
-					},
-				},
-			},
-		})
 		vim.lsp.config("rust_analyzer", {
 			capabilities = {
 				experimental = {
