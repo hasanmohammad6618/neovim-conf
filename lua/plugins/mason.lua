5383b86bc296963bb58f0f2a3b63d4cd49a68144
Mohammad Hasan <hasanmohammad6618@gmail.com>
2026-06-30 12:09:33 +0530

    fix typo in mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -40,7 +40,7 @@ return {
         })
         local lsp_pkg = {
             "deno", "rust-analyzer", "clangd", "emmylua_ls", "json-lsp", "csskit", "tombi", "ruff", "ty", "marksman",
-            "tinymist", "kpm-lsp", "superhtml", "emmet-language-server", "lemminx"
+            "tinymist", "kmp-lsp", "superhtml", "emmet-language-server", "lemminx"
         }
 
         local other_pkg = { "typstyle", "uv", "gitui" }
