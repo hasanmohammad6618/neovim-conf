6e097cec23ce1aec215797134c5df9e5562f3ef5
Mohammad Hasan <hasanmohammad6618@gmail.com>
2026-08-14 06:04:03 +0530

    add wezterm types as dependency

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -2,11 +2,13 @@ return {
     {
         "hasanmohammad6618/lazydev.nvim",
         ft = "lua", -- only load on lua files
+        dependencies = { 'DrKJeff16/wezterm-types' },
         opts = {
             library = {
                 "lazy.nvim",
                 -- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
+                { path = 'wezterm-types', mods = { 'wezterm' } },
                 { path = "${3rd}/luv/library", words = { "vim%.uv" } }
             }
         }
